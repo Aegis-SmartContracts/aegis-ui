7f0880906eb0b8e22d3ee80cce535c7a78c25d2f
Debian <admin@ip-172-31-42-2.eu-central-1.compute.internal>
2018-05-03 09:19:20 +0000

    #16068 - aegis fix

--- web/app/components/Dashboard/Dashboard.jsx
+++ web/app/components/Dashboard/Dashboard.jsx
@@ -236,19 +236,7 @@ class Dashboard extends React.Component {
                                         <Translate content="account.create_new" />
                                     </div>
 
-                                    <div className="button create-account" onClick={() => {
-                                        SettingsActions.changeSetting({setting: "passwordLogin", value: true});
-                                        WalletUnlockActions.unlock.defer();
-                                    }}>
-                                        <Translate content="account.password_login" />
-                                    </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="grid-container small-12 medium-7" style={{paddingTop: 44}}>
-                            <Translate content="exchange.featured" component="h4" style={{paddingLeft: 30}}/>
-                            <div className="grid-block small-up-1 large-up-3 xlarge-up-4 no-overflow fm-outer-container">
-                                {markets}
                             </div>
                         </div>
                     </div>
@@ -259,12 +247,6 @@ class Dashboard extends React.Component {
         return (
             <div ref="wrapper" className="grid-block page-layout vertical">
                 <div ref="container" className="grid-container" style={{padding: "25px 10px 0 10px"}}>
-                    <div className="block-content-header" style={{marginBottom: 15}}>
-                    <Translate content="exchange.featured"/>
-                    </div>
-                    <div className="grid-block small-up-1 medium-up-3 large-up-4 no-overflow fm-outer-container">
-                        {markets}
-                    </div>
 
                     {accountCount ? <div className="generic-bordered-box" style={{marginBottom: 5}}>
                         <div className="block-content-header" style={{marginBottom: 15}}>
